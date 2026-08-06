2c31196d3a94424dbc6e31a1faef13dfe01eb96e
FacuuChavez <facuunlam08@gmail.com>
2026-08-06 16:55:48 -0300

    commit inicial

---
+++ src/test/java/com/facuulra/bazar_api/BazarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facuulra.bazar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
